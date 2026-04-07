eb6ace804d19f1fc0d732cf4e8be2ab0137a703a
Wasit-karimi <wasitkarimi0@gmail.com>
2026-04-08 02:34:50 +0430

    uploaded my cv

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -28,7 +28,7 @@ const AboutSection = () => {
               Get in Touch
             </a>
             <a
-              download={}
+              download="../../public/wasitkarimi-cv.docx"
               className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
             >
               Download CV
