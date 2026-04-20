53edb6c6f7fe8e2a2332a2eee30c8b930a7038c8
Wasit-karimi <wasitkarimi0@gmail.com>
2026-04-20 12:01:23 +0430

    It was ok

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -127,11 +127,10 @@ export const ContactSection = () => {
 
           <div
             className="bg-card p-8 rounded-lg shadow-xs"
-            onSubmit={handleSubmit}
           >
             <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
 
-            <form className="space-y-6">
+            <form onSubmit={handleSubmit} className="space-y-6">
               <div>
                 <label
                   htmlFor="name"
